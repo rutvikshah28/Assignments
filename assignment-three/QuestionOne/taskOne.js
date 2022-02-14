3db9072e343d6d823f9adfa55c76ccff38f683aa
rutvikshah28 <rutvikshah28@hotmail.com>
2022-02-14 12:05:36 -0700

    feat(Assignments): Assignment3 work start. Question One initial solution done.

---
+++ assignment-three/QuestionOne/taskOne.js
@@ -0,0 +1,95 @@
+/*
+    Task One:
+        1. Create a 3+ state union type -- 2+ states contain a value
+        2. Build constructors, discriminated union, typeguards and other access methods
+        3. fold and/or match
+        -- Extensions
+        4. map, apply, other list like helper methods
+        5. Partial match with a default value
+
+        Example: ProgressOption<T> = NotStarted | Finished<T> | Progress<{val: number}> | Paused<{val: number}>;
+*/
+var notStarted = function () {
+    return { __tag: "notStarted" };
+};
+var finished = function (fin) {
+    return {
+        __tag: "finished",
+        val: fin
+    };
+};
+var progress = function (p) {
+    return {
+        __tag: "progress",
+        val: { val: p }
+    };
+};
+var isNotStarted = function (p) { return p.__tag === "notStarted"; };
+var isFinished = function (p) { return p.__tag === "finished"; };
+var isProgress = function (p) { return p.__tag === "progress"; };
+var ns = notStarted();
+var fin = finished(100);
+var prog = progress(25);
+var fold = function (p, d) {
+    if (isNotStarted(p)) {
+        return d();
+    }
+    if (isFinished(p)) {
+        return p.val;
+    }
+    if (isProgress(p)) {
+        return p.val.val;
+    }
+};
+console.log("Fold - Progress(25): ", fold(prog, function () { return "default"; }));
+var match = function (p, f, g, d) {
+    if (isNotStarted(p)) {
+        return d();
+    }
+    else if (isFinished(p)) {
+        return f(p.val);
+    }
+    else if (isProgress(p)) {
+        return g(p.val.val);
+    }
+};
+console.log("NotStarted: ", match(ns, function (t) { return t + "%. Task is finished."; }, function (t) { return t.toString() + "%. Task is in progress."; }, function () { return "0%. Task has not started yet."; }));
+console.log("Progress: ", match(prog, function (t) { return t + "%. Task is finished."; }, function (t) { return t.toString() + "%. Task is in progress."; }, function () { return "0%. Task has not started yet."; }));
+console.log("Finished: ", match(fin, function (t) { return t + "%. Task is finished."; }, function (t) { return t.toString() + "%. Task is in progress."; }, function () { return "0%. Task has not started yet."; }));
+var map = function (p, f, g) {
+    if (isNotStarted(p)) {
+        return notStarted();
+    }
+    else if (isFinished(p)) {
+        return finished(f(p.val));
+    }
+    else if (isProgress(p)) {
+        return progress(g(p.val.val));
+    }
+};
+console.log("map: ", map(fin, function (val) { return val + 200; }, function (val) { return val + 111; }));
+//find for arrays in js returns undefined if nothing is found
+var find = function (p, val) {
+    if (isFinished(p)) {
+        return p.val === val ? p.val : undefined;
+    }
+    else if (isProgress(p)) {
+        return p.val.val === val ? p.val : undefined;
+    }
+    return undefined;
+};
+console.log("Find(100): ", find(fin, 100));
+var isEmpty = function (p) { return isNotStarted(p) ? true : false; };
+console.log("isEmpty - NotStarted: ", isEmpty(ns));
+var contains = function (p, val) {
+    if (isFinished(p)) {
+        return p.val === val;
+    }
+    else if (isProgress(p)) {
+        return p.val.val === val;
+    }
+    else {
+        return false;
+    }
+};
+console.log("Contains(25) with Finished", contains(fin, 25));
---
+++ assignment-three/QuestionOne/taskOne.ts
@@ -0,0 +1,182 @@
+/*
+    Task One:
+        1. Create a 3+ state union type -- 2+ states contain a value
+        2. Build constructors, discriminated union, typeguards and other access methods
+        3. fold and/or match
+        -- Extensions
+        4. map, apply, other list like helper methods
+        5. Partial match with a default value
+
+        Example: ProgressOption<T> = NotStarted | Finished<T> | Progress<{val: number}> | Paused<{val: number}>;
+*/
+
+type NotStarted = {__tag: "notStarted"};
+type Finished<T> = {__tag: "finished", val: T};
+type Progress = {__tag: "progress", val: {val: number}};
+
+type ProgressOption<T> = NotStarted | Finished<T> | Progress;
+
+const notStarted = <T>(): ProgressOption<T> => {
+    return {__tag: "notStarted"};
+}
+
+const finished = <T>(fin: T): ProgressOption<T> => {
+    return {
+        __tag: "finished",
+        val: fin
+    }
+}
+
+const progress = <T>(p: number): ProgressOption<T> => {
+    return {
+        __tag: "progress",
+        val: {val: p}
+    }
+}
+
+const isNotStarted = <T>(p: ProgressOption<T>): p is NotStarted => p.__tag === "notStarted";
+const isFinished = <T>(p: ProgressOption<T>): p is Finished<T> => p.__tag === "finished";
+const isProgress = <T>(p: ProgressOption<T>): p is Progress => p.__tag === "progress";
+
+const ns = notStarted();
+const fin = finished(100);
+const prog = progress(25);
+
+const fold = <T>(p: ProgressOption<T>, d: () => T | number): T | number => {
+    if(isNotStarted(p)){
+        return d();
+    }
+    if(isFinished(p)){
+        return p.val;
+    }
+    if(isProgress(p)){
+        return p.val.val;
+    }
+}
+
+
+console.log(
+    "Fold - Progress(25): ",
+    fold(
+        prog,
+        () => "default"
+    )
+);
+
+const match = <T, T2>(
+    p: ProgressOption<T>,
+    f: (t: T) => T2,
+    g: (t: number) => T2,
+    d: () => T2
+    ): T2 => {
+        if(isNotStarted(p)){
+            return d();
+        }
+        else if(isFinished(p)){
+            return f(p.val);
+        }
+        else if(isProgress(p)){
+            return g(p.val.val);
+        }
+    }
+
+
+
+console.log(
+    "NotStarted: ",
+    match(
+    ns,
+    (t) => t + "%. Task is finished.",
+    (t) => t.toString() + "%. Task is in progress.",
+    () => "0%. Task has not started yet."
+    )
+);
+
+console.log(
+    "Progress: ",
+    match(
+    prog,
+    (t) => t + "%. Task is finished.",
+    (t) => t.toString() + "%. Task is in progress.",
+    () => "0%. Task has not started yet."
+    )
+);
+
+console.log(
+    "Finished: ",
+    match(
+    fin,
+    (t) => t + "%. Task is finished.",
+    (t) => t.toString() + "%. Task is in progress.",
+    () => "0%. Task has not started yet."
+    )
+);
+
+
+const map = <T, T2>(p: ProgressOption<T>, f: (val: T) => T2, g: (val: number) => number): ProgressOption<T2> => {
+    if(isNotStarted(p)){
+        return notStarted();
+    }
+    else if(isFinished(p)){
+        return finished(f(p.val));
+    }
+    else if(isProgress(p)){
+        return progress(g(p.val.val));
+    }
+}
+
+console.log(
+    "map: ",
+    map(
+        fin,
+        (val) => val + 200,
+        (val) => val + 111,
+    )
+);
+
+//find for arrays in js returns undefined if nothing is found
+const find = <T>(p: ProgressOption<T>, val: T | number) => {
+    if(isFinished(p)){
+        return p.val === val ? p.val : undefined;
+    }
+    else if(isProgress(p)){
+        return p.val.val === val ? p.val : undefined;
+    }
+
+    return undefined;
+}
+
+console.log(
+    "Find(100): ",
+    find(
+        fin,
+        100
+    )
+);
+
+const isEmpty = <T>(p: ProgressOption<T>) => isNotStarted(p) ? true : false;
+
+console.log(
+    "isEmpty - NotStarted: ",
+    isEmpty(ns)
+);
+
+const contains = <T>(p: ProgressOption<T>, val: T | number) => {
+    if(isFinished(p)){
+        return p.val === val;
+    }
+    else if(isProgress(p)){
+        return p.val.val === val;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(
+    "Contains(25) with Finished",
+    contains(
+        fin,
+        25
+    )
+);
