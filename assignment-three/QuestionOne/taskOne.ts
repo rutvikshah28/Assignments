67a10d7f77881a12d926d7cf37b4d71e1559c51e
rutvikshah28 <rutvikshah28@hotmail.com>
2022-02-15 11:17:58 -0700

    feat(Assignments): Q1 make the type to four state instead of three

--- assignment-three/QuestionOne/taskOne.ts
+++ assignment-three/QuestionOne/taskOne.ts
@@ -15,7 +15,8 @@
 type NotStarted = {__tag: "notStarted"};
 type Finished<T> = {__tag: "finished", val: T};
 type Progress = {__tag: "progress", val: {val: number}};
-type ProgressOption<T> = NotStarted | Finished<T> | Progress;
+type Paused = {__tag: "paused", val: boolean};
+type ProgressOption<T> = NotStarted | Finished<T> | Progress | Paused;
 
 
 const notStarted = <T>(): ProgressOption<T> => {
@@ -33,17 +34,25 @@ const progress = <T>(p: number): ProgressOption<T> => {
         val: {val: p}
     }
 }
+const paused = <T>(b: boolean): ProgressOption<T> => {
+    return {
+        __tag: "paused",
+        val: b
+    }
+}
 
 
 const isNotStarted = <T>(p: ProgressOption<T>): p is NotStarted => p.__tag === "notStarted";
 const isFinished = <T>(p: ProgressOption<T>): p is Finished<T> => p.__tag === "finished";
 const isProgress = <T>(p: ProgressOption<T>): p is Progress => p.__tag === "progress";
+const isPaused = <T>(p: ProgressOption<T>): p is Paused => p.__tag === "paused";
 
 const ns = notStarted();
 const fin = finished(100);
 const prog = progress(25);
+const psd = paused(true);
 
-const fold = <T>(p: ProgressOption<T>, d: () => T | number): T | number => {
+const fold = <T>(p: ProgressOption<T>, d: () => T | number): T | number | boolean => {
     if(isNotStarted(p)){
         return d();
     }
@@ -52,6 +61,9 @@ const fold = <T>(p: ProgressOption<T>, d: () => T | number): T | number => {
     }
     if(isProgress(p)){
         return p.val.val;
+    }
+    if(isPaused(p)){
+        return p.val;
     }
 }
 console.log(
@@ -66,6 +78,7 @@ const match = <T, T2>(
     p: ProgressOption<T>,
     f: (t: T) => T2,
     g: (t: number) => T2,
+    h: (t: boolean) => T2,
     d: () => T2
     ): T2 => {
         if(isNotStarted(p)){
@@ -77,6 +90,9 @@ const match = <T, T2>(
         else if(isProgress(p)){
             return g(p.val.val);
         }
+        else if(isPaused(p)){
+            return h(p.val);
+        }
     }
 console.log(
     "NotStarted: ",
@@ -84,6 +100,7 @@ console.log(
     ns,
     (t) => t + "%. Task is finished.",
     (t) => t.toString() + "%. Task is in progress.",
+    (t) => t === true ? "Task is paused" : "Task is not paused",
     () => "0%. Task has not started yet."
     )
 );
@@ -93,6 +110,7 @@ console.log(
     prog,
     (t) => t + "%. Task is finished.",
     (t) => t.toString() + "%. Task is in progress.",
+    (t) => t === true ? "Task is paused" : "Task is not paused",
     () => "0%. Task has not started yet."
     )
 );
@@ -102,12 +120,22 @@ console.log(
     fin,
     (t) => t + "%. Task is finished.",
     (t) => t.toString() + "%. Task is in progress.",
+    (t) => t === true ? "Task is paused" : "Task is not paused",
     () => "0%. Task has not started yet."
     )
 );
+console.log(
+    "Paused: ",
+    match(
+        psd,
+        (t) => t + "%. Task is finished.",
+        (t) => t.toString() + "%. Task is in progress.",
+        (t) => t === true ? "Task is paused" : "Task is not paused",
+        () => "0%. Task has not started yet."
+    )
+);
 
-
-const map = <T, T2>(p: ProgressOption<T>, f: (val: T) => T2, g: (val: number) => number): ProgressOption<T2> => {
+const map = <T, T2>(p: ProgressOption<T>, f: (val: T) => T2, g: (val: number) => number, h: (val: boolean) => boolean): ProgressOption<T2> => {
     if(isNotStarted(p)){
         return notStarted();
     }
@@ -117,6 +145,9 @@ const map = <T, T2>(p: ProgressOption<T>, f: (val: T) => T2, g: (val: number) =>
     else if(isProgress(p)){
         return progress(g(p.val.val));
     }
+    else if(isPaused(p)){
+        return paused(h(p.val));
+    }
 }
 console.log(
     "map: ",
@@ -124,16 +155,20 @@ console.log(
         fin,
         (val) => val + 200,
         (val) => val + 111,
+        (val) => !val
     )
 );
 
 //find for arrays in js returns undefined if nothing is found
-const find = <T>(p: ProgressOption<T>, val: T | number) => {
+const find = <T>(p: ProgressOption<T>, val: T | number | boolean) => {
     if(isFinished(p)){
         return p.val === val ? p.val : undefined;
     }
     else if(isProgress(p)){
         return p.val.val === val ? p.val : undefined;
+    }
+    else if(isPaused(p)){
+        return p.val === val ? p.val : undefined;
     }
 
     return undefined;
@@ -152,12 +187,15 @@ console.log(
     isEmpty(ns)
 );
 
-const contains = <T>(p: ProgressOption<T>, val: T | number) => {
+const contains = <T>(p: ProgressOption<T>, val: T | number | boolean) => {
     if(isFinished(p)){
         return p.val === val;
     }
     else if(isProgress(p)){
         return p.val.val === val;
+    }
+    else if(isPaused(p)){
+        return p.val === val;
     }
     else{
         return false;
