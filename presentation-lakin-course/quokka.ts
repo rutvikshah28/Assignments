8ce5f8972333c54332fea2a958ae49fa1f647bfc
Rutvik Shah <rutvikshah28@hotmail.com>
2022-02-24 15:25:59 -0700

    feat(Assignments): Ready for lakin presentation

--- presentation-lakin-course/quokka.ts
+++ presentation-lakin-course/quokka.ts
@@ -1,5 +1,27 @@
-const baseUrl = "https://jsonplaceholder.typicode.com/";
+import fetch from 'node-fetch';
 
-const comments = fetch(baseUrl + "comments");
+const comments = async() => {
+    return await fetch("https://jsonplaceholder.typicode.com/posts/1", undefined).then(results => results.json()).then(json => console.log(json));
+}
 
-console.log(comments);
+const posts = async() => {
+    return await fetch("https://jsonplaceholder.typicode.com/posts/1", undefined).then(results => results.json()).then(json => console.log(json));
+}
+
+const todos = async() => {
+    return await fetch("https://jsonplaceholder.typicode.com/todos/1", undefined).then(results => results.json()).then(json => console.log(json));
+}
+
+const users = async() => {
+    return await fetch("https://jsonplaceholder.typicode.com/users", undefined).then(results => results.json()).then(json => console.log(json));
+}
+
+const albums = async() => {
+    return await fetch("https://jsonplaceholder.typicode.com/albums", undefined).then(results => results.json()).then(json => console.log(json));
+}
+
+console.log(comments())
+console.log(posts());
+console.log(todos());
+console.log(users());
+console.log(albums());
